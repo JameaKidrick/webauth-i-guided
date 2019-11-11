b088c7f72ab2a098c97f29e893afe36818768625
JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
2019-11-11 12:36:08 -0500

    did not finish yet

--- api/api-router.js
+++ api/api-router.js
@@ -3,11 +3,31 @@ const router = require('express').Router();
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+// const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
+
 router.use('/auth', authRouter);
 router.use('/users', usersRouter);
 
 router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
 });
+
+router.post('/hash', (req, res) => {
+  // read a password from the body
+  // hash the password using bcrypt.js
+  // return it to the user in an object that looks like
+  // { password: 'original password', hash: 'hashed password' }
+  const myPlaintextPassword = req.body;
+  bcrypt.genSalt(saltRounds, (err, salt) => {
+    bcrypt.hash(myPlaintextPassword, salt, (err, hash) => {
+      console.log(myPlaintextPassword.hash)
+      
+    })
+    
+  })
+})
 
 module.exports = router;
---
+++ notes.md
@@ -0,0 +1,4 @@
+WEBSITES:
+grc.com/haystack.htm
+https://www.npmjs.com/package/bcryptjs
+https://www.cloudflare.com/learning/security/threats/owasp-top-10/
